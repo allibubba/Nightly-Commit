275857e72b72d455153af46207ad190a456b2c27
jackson <jackson.oates@gmail.com>
2012-04-05 13:02:53 -0700

    updated recursive directory loop

---
+++ projectPush.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+####################################
+#
+# Nightly project commit
+# Recursively loop through directory and commit any project changes
+# To enable desktop notifications, not really needed, but they're fucking cool
+# $ sudo apt-get install libnotify-bin
+# TODO: Add ability to pass in project directory in via cron task
+####################################
+
+
+SCRIPT=`readlink -f $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+#PROJECTS=( $( cat $SCRIPTPATH/directory_list.txt ) )
+#PROJ=( $( cat $PROJECTPATH/ ) )
+
+PROJECTPATH=$HOME/Projects
+PROJECTS=( $(find $PROJECTPATH -maxdepth 1 -type d -printf '%P\n') )
+
+runCommit () {
+  git add .
+  git commit -m"nightly commit: "$1
+  git push
+  notify-send $1
+}
+
+for proj in ${PROJECTS[@]}
+do
+  dir = $HOME/Projects/$proj
+  cd $HOME/Projects/$proj
+  if [ -d $HOME/Projects/$proj/.git ] && [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]]
+    then
+      runCommit $proj
+    else
+      echo "!! NOPE"
+  fi
+done
+
+echo "Finished running projectPush at `date`" >> $SCRIPTPATH/task.log
+
+
